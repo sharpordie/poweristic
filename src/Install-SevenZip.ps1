26421307b45ee9c04cbd2b1bac2540ef7a78d8eb
sharpordie <sharpordie@outlook.com>
2020-12-31 15:54:59 +0100

    feat: add new Install-SevenZip script

---
+++ src/Install-SevenZip.ps1
@@ -0,0 +1,11 @@
+#Requires -RunAsAdministrator
+
+# Download and install 7-Zip silently.
+$website = 'https://www.7-zip.org/download.html'
+$content = (New-Object System.Net.WebClient).DownloadString($website)
+$pattern = 'Download 7-Zip ([\d.]+)'
+$version = ([Regex]::Matches($content, $pattern).Groups[1].Value).Replace('.', '')
+$address = if ([System.Environment]::Is64BitOperatingSystem) { "https://7-zip.org/a/7z$version-x64.msi" } else { "https://7-zip.org/a/7z$version.msi" }
+$program = [System.IO.Path]::Combine($env:TEMP, [System.IO.Path]::GetFileName($address))
+(New-Object System.Net.WebClient).DownloadFile($address, $program)
+Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i `"$program`" /qn" -NoNewWindow -Wait
