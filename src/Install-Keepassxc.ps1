5c4ce3bfcd55d3b93bf12cd3fb9255984b8ffe1b
sharpordie <sharpordie@outlook.com>
2021-01-01 14:01:17 +0100

    feat: add new Install-Keepassxc script

---
+++ src/Install-Keepassxc.ps1
@@ -0,0 +1,9 @@
+#Requires -RunAsAdministrator
+
+# Download and install KeePassXC silently.
+$website = 'https://api.github.com/repos/keepassxreboot/keepassxc/releases/latest'
+$version = (Invoke-WebRequest -Uri $website -UseBasicParsing | ConvertFrom-Json)[0].tag_name
+$address = "https://github.com/keepassxreboot/keepassxc/releases/download/$version/KeePassXC-$version-Win64.msi"
+$program = [System.IO.Path]::Combine($env:TEMP, [System.IO.Path]::GetFileName($address))
+(New-Object System.Net.WebClient).DownloadFile($address, $program)
+Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i `"$program`" /qn" -NoNewWindow -Wait
