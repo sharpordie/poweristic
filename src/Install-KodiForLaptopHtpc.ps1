d9583acef1e5a6a7743f36370a30aab9d386c3fd
sharpordie <sharpordie@outlook.com>
2021-01-04 11:47:36 +0100

    feat: add new Install-KodiForLaptopHtpc script

---
+++ src/Install-KodiForLaptopHtpc.ps1
@@ -0,0 +1,59 @@
+function Add-KodiToWindowsStartup {
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Kodi' -Value "$env:ProgramFiles\Kodi\kodi.exe"
+}
+
+function Install-Kodi {
+    $website = 'https://mirrors.kodi.tv/releases/windows/win64/?C=M&O=D'
+    $content = (New-Object System.Net.WebClient).DownloadString($website)
+    $pattern = 'kodi-([\w.-]+)-x64'
+    $version = [Regex]::Matches($content, $pattern).Groups[1].Value
+    $address = "https://mirrors.kodi.tv/releases/windows/win64/kodi-$version-x64.exe"
+    $program = [System.IO.Path]::Combine($env:TEMP, [System.IO.Path]::GetFileName($address))
+    (New-Object System.Net.WebClient).DownloadFile($address, $program)
+    Start-Process -FilePath `"$program`" -ArgumentList '/S' -NoNewWindow -Wait
+}
+
+function Set-DefaultSettings {
+    $settings = "$env:APPDATA\Kodi\userdata\guisettings.xml"
+    if (Test-Path -Path $settings) {
+        [xml] $document = Get-Content -Path $settings
+        $document.SelectSingleNode('//*[@id="addons.unknownsources"]').default = 'false'
+        $document.SelectSingleNode('//*[@id="addons.unknownsources"]').'#text' = 'true'
+        $document.SelectSingleNode('//*[@id="locale.country"]').default = 'false'
+        $document.SelectSingleNode('//*[@id="locale.country"]').'#text' = 'Belgique'
+        $document.SelectSingleNode('//*[@id="locale.language"]').default = 'false'
+        $document.SelectSingleNode('//*[@id="locale.language"]').'#text' = 'resource.language.fr_fr'
+        $document.SelectSingleNode('//*[@id="videoplayer.adjustrefreshrate"]').default = 'false'
+        $document.SelectSingleNode('//*[@id="videoplayer.adjustrefreshrate"]').'#text' = '2'
+        $document.SelectSingleNode('//*[@id="videoplayer.usedisplayasclock"]').default = 'false'
+        $document.SelectSingleNode('//*[@id="videoplayer.usedisplayasclock"]').'#text' = 'true'
+        $document.SelectSingleNode('//*[@id="services.webserver"]').default = 'false'
+        $document.SelectSingleNode('//*[@id="services.webserver"]').'#text' = 'true'
+        $document.SelectSingleNode('/settings/defaultvideosettings/centermixlevel').'#text' = '5'
+        $document.SelectSingleNode('/settings/defaultvideosettings/scalingmethod').'#text' = '9' # Lanczos3
+        $document.SelectSingleNode('/settings/defaultvideosettings/tonemapmethod').'#text' = '3' # Hable
+        $document.Save($settings)
+    }
+}
+
+function Set-DoNothingAsCloseLidAction {
+    powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+    powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+    powercfg -s SCHEME_CURRENT
+}
+
+function Set-UltimatePowerPlan {
+    $uuid = (powercfg -l | ForEach-Object { if ($_.contains('Ultimate Performance')) { $_.split()[3] } })
+    if ([string]::IsNullOrEmpty($uuid)) { Start-Process -FilePath 'powercfg.exe' -ArgumentList '-duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61' -NoNewWindow -Wait }
+    $uuid = (powercfg -l | ForEach-Object { if ($_.contains('Ultimate Performance')) { $_.split()[3] } })
+    Start-Process -FilePath 'powercfg.exe' -ArgumentList "-s $uuid" -NoNewWindow -Wait
+}
+
+#Requires -RunAsAdministrator
+
+Rename-Computer -NewName 'HTPC'
+Set-UltimatePowerPlan
+Set-DoNothingAsCloseLidAction
+Install-Kodi
+Add-KodiToWindowsStartup
+Set-DefaultSettings
