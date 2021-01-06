c21711977a3b1ab141da193690d619f47e773046
sharpordie <sharpordie@outlook.com>
2021-01-06 13:22:16 +0100

    refactor: split functions into smaller ones

--- src/Install-KodiForLaptopHtpc.ps1
+++ src/Install-KodiForLaptopHtpc.ps1
@@ -13,37 +13,87 @@ function Install-Kodi {
     Start-Process -FilePath `"$program`" -ArgumentList '/S' -NoNewWindow -Wait
 }
 
-function Set-KodiSettings {
-    # Reset the default guisettings.xml file.
+function Reset-KodiSettings {
     $settings = "$env:APPDATA\Kodi\userdata\guisettings.xml"
     Remove-Item -Path $settings -ErrorAction SilentlyContinue
     Start-Process -FilePath "$env:ProgramFiles\Kodi\kodi.exe" -WindowStyle Minimized
     Start-Sleep -Seconds 10
     Stop-Process -Name 'Kodi'
     Start-Sleep -Seconds 5
+}
 
-    # Edit the guisettings.xml file.
+function Set-KodiCountry {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Country
+    )
+    $settings = "$env:APPDATA\Kodi\userdata\guisettings.xml"
     [xml] $document = Get-Content -Path $settings
-    $document.SelectSingleNode('//*[@id="addons.unknownsources"]').default = 'false'
+    try { $document.SelectSingleNode('//*[@id="locale.country"]').default = 'false' } catch {}
+    $document.SelectSingleNode('//*[@id="locale.country"]').InnerText = "$Country"
+    $document.Save($settings)
+}
+
+function Set-KodiDefaultSettings {
+    Reset-KodiSettings
+    $settings = "$env:APPDATA\Kodi\userdata\guisettings.xml"
+    [xml] $document = Get-Content -Path $settings
+    try { $document.SelectSingleNode('//*[@id="addons.unknownsources"]').default = 'false' } catch {}
     $document.SelectSingleNode('//*[@id="addons.unknownsources"]').InnerText = 'true'
-    $document.SelectSingleNode('//*[@id="locale.country"]').default = 'false'
-    $document.SelectSingleNode('//*[@id="locale.country"]').InnerText = 'Belgique'
-    $document.SelectSingleNode('//*[@id="locale.language"]').default = 'false'
-    $document.SelectSingleNode('//*[@id="locale.language"]').InnerText = 'resource.language.fr_fr'
-    $document.SelectSingleNode('//*[@id="videoplayer.adjustrefreshrate"]').default = 'false'
+    try { $document.SelectSingleNode('//*[@id="videoplayer.adjustrefreshrate"]').default = 'false' } catch {}
     $document.SelectSingleNode('//*[@id="videoplayer.adjustrefreshrate"]').InnerText = '2'
-    $document.SelectSingleNode('//*[@id="videoplayer.usedisplayasclock"]').default = 'false'
+    try { $document.SelectSingleNode('//*[@id="videoplayer.usedisplayasclock"]').default = 'false' } catch {}
     $document.SelectSingleNode('//*[@id="videoplayer.usedisplayasclock"]').InnerText = 'true'
-    $document.SelectSingleNode('//*[@id="services.webserver"]').default = 'false'
-    $document.SelectSingleNode('//*[@id="services.webserver"]').InnerText = 'true'
-    $document.SelectSingleNode('//*[@id="services.webserverpassword"]').default = 'false'
-    $document.SelectSingleNode('//*[@id="services.webserverpassword"]').InnerText = '1234'
     $document.SelectSingleNode('/settings/defaultvideosettings/centermixlevel').InnerText = '5'
     $document.SelectSingleNode('/settings/defaultvideosettings/scalingmethod').InnerText = '9' # Lanczos3
     $document.SelectSingleNode('/settings/defaultvideosettings/tonemapmethod').InnerText = '3' # Hable
     $document.Save($settings)
 }
 
+function Set-KodiLanguage {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Language
+    )
+    # Get the latest release name.
+    $website = 'http://mirrors.kodi.tv/addons/?C=M&O=D'
+    $content = (New-Object System.Net.WebClient).DownloadString($website)
+    $pattern = 'title="([\w]+)"'
+    $release = [Regex]::Matches($content, $pattern).Groups[1].Value
+
+    # Get the latest available language version.
+    $website = "http://mirrors.kodi.tv/addons/$release/resource.language.$Language/?C=M&O=D"
+    $content = (New-Object System.Net.WebClient).DownloadString($website)
+    $pattern = "href=`"resource.language.$Language-([\d.]+).zip`""
+    $version = [Regex]::Matches($content, $pattern).Groups[1].Value
+    $address = "http://mirrors.kodi.tv/addons/$release/resource.language.$Language/resource.language.$Language-$version.zip"
+    $archive = [System.IO.Path]::Combine($env:TEMP, [System.IO.Path]::GetFileName($address))
+    (New-Object System.Net.WebClient).DownloadFile($address, $archive)
+
+    # Extract to the addons directory
+    Expand-Archive -Path $archive -DestinationPath "$env:APPDATA\Kodi\addons" -Force
+
+    # Edit the guisettings.xml file.
+    $settings = "$env:APPDATA\Kodi\userdata\guisettings.xml"
+    [xml] $document = Get-Content -Path $settings
+    try { $document.SelectSingleNode('//*[@id="locale.language"]').default = 'false' } catch {}
+    $document.SelectSingleNode('//*[@id="locale.language"]').InnerText = "resource.language.$Language"
+    $document.Save($settings)
+}
+
+function Set-KodiWebServerNoPassword {
+    $settings = "$env:APPDATA\Kodi\userdata\guisettings.xml"
+    [xml] $document = Get-Content -Path $settings
+    try { $document.SelectSingleNode('//*[@id="services.webserver"]').default = 'false' } catch {}
+    $document.SelectSingleNode('//*[@id="services.webserver"]').InnerText = 'true'
+    try { $document.SelectSingleNode('//*[@id="services.webserverauthentication"]').default = 'false' } catch {}
+    $document.SelectSingleNode('//*[@id="services.webserverauthentication"]').InnerText = 'false'
+    $document.Save($settings)
+}
 function Set-KodiSources {
     # Make sure to create the media directories.
     $rootMediaDirectory = 'C:\MyMedia\'
@@ -51,11 +101,11 @@ function Set-KodiSources {
     $moviesMediaDirectory = 'C:\MyMedia\Movies\'
     $picturesMediaDirectory = 'C:\MyMedia\Pictures\'
     $seriesMediaDirectory = 'C:\MyMedia\Series\'
-    New-Item -ItemType Directory -Force -Path $rootMediaDirectory
-    New-Item -ItemType Directory -Force -Path $albumsMediaDirectory
-    New-Item -ItemType Directory -Force -Path $moviesMediaDirectory
-    New-Item -ItemType Directory -Force -Path $picturesMediaDirectory
-    New-Item -ItemType Directory -Force -Path $seriesMediaDirectory
+    New-Item -ItemType Directory -Path $rootMediaDirectory -Force
+    New-Item -ItemType Directory -Path $albumsMediaDirectory -Force
+    New-Item -ItemType Directory -Path $moviesMediaDirectory -Force
+    New-Item -ItemType Directory -Path $picturesMediaDirectory -Force
+    New-Item -ItemType Directory -Path $seriesMediaDirectory -Force
 
     # Make sure to reset the xml file.
     $settings = "$env:APPDATA\Kodi\userdata\sources.xml"
@@ -139,13 +189,13 @@ function Set-KodiSources {
     $document.Save($settings)
 }
 
-function Set-DoNothingAsCloseLidAction {
+function Set-SystemDoNothingAsCloseLidAction {
     powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
     powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
     powercfg -s SCHEME_CURRENT
 }
 
-function Set-UltimatePowerPlan {
+function Set-SystemUltimatePowerPlan {
     $uuid = (powercfg -l | ForEach-Object { if ($_.contains('Ultimate Performance')) { $_.split()[3] } })
     if ([string]::IsNullOrEmpty($uuid)) { Start-Process -FilePath 'powercfg.exe' -ArgumentList '-duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61' -NoNewWindow -Wait }
     $uuid = (powercfg -l | ForEach-Object { if ($_.contains('Ultimate Performance')) { $_.split()[3] } })
@@ -155,9 +205,13 @@ function Set-UltimatePowerPlan {
 #Requires -RunAsAdministrator
 
 # Rename-Computer -NewName 'HTPC'
-Set-UltimatePowerPlan
-Set-DoNothingAsCloseLidAction
 Install-Kodi
 Add-KodiToWindowsStartup
-Set-KodiSettings
+Set-KodiDefaultSettings
+Set-KodiCountry -Country 'Belgique'
+Set-KodiLanguage -Language 'fr_fr'
 # Set-KodiSources
+Set-KodiWebServerNoPassword
+Set-SystemUltimatePowerPlan
+Set-SystemDoNothingAsCloseLidAction
+# Restart-Computer
