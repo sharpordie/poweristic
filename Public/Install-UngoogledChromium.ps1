c427fba3dc4887e2a2ba317078ae671d2d2a3055
sharpordie <72373746+sharpordie@users.noreply.github.com>
2021-04-27 12:20:50 +0200

    fix: enable the downloading

--- Public/Install-UngoogledChromium.ps1
+++ Public/Install-UngoogledChromium.ps1
@@ -7,7 +7,7 @@ $pattern = '\d\">\s*([\d.-]+)<'
 $version = [Regex]::Matches($content, $pattern).Groups[1].Value
 $address = "https://github.com/tangalbert919/ungoogled-chromium-binaries/releases/download/$version/ungoogled-chromium_$version.1_installer-x64.exe"
 $program = [System.IO.Path]::Combine($env:TEMP, [System.IO.Path]::GetFileName($address))
-# (New-Object System.Net.WebClient).DownloadFile($address, $program)
+(New-Object System.Net.WebClient).DownloadFile($address, $program)
 if (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium") {
     Start-Process -FilePath `"$program`" -ArgumentList '--do-not-launch-chrome' -NoNewWindow -Wait
 }
